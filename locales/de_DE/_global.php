266f1c0dcffc7d5c883727e2b3663eacd6d074f7
Keith Bowes <zooplah@gmail.com>
2015-03-30 00:12:22 -0400

    Updated translations

--- locales/de_DE/_global.php
+++ locales/de_DE/_global.php
@@ -6,13 +6,15 @@
 if( !defined('EVO_MAIN_INIT') ) die( 'Please, do not access this page directly.' );
 
 $trans['de_DE'] = array(
-'__meta__' => array('format_version'=>1, 'charset'=>'iso-8859-1'),
-'' => "Project-Id-Version: b 2evolution\nReport-Msgid-Bugs-To: http://fplanque.net/\nPOT-Creation-Date: 2015-03-29 19:43-0400\nPO-Revision-Date: 2015-03-29 19:50-0400\nLast-Translator: Keith <keith@localhost>\nLanguage-Team: German\nLanguage: de\nMIME-Version: 1.0\nContent-Type: text/plain; charset=ISO-8859-1\nContent-Transfer-Encoding: 8bit\nPlural-Forms: nplurals=2; plural=(n != 1);\n",
+'__meta__' => array('format_version'=>1, 'charset'=>'utf-8'),
+'' => "Project-Id-Version: b 2evolution\nReport-Msgid-Bugs-To: http://fplanque.net/\nPOT-Creation-Date: 2015-03-30 00:04-0400\nPO-Revision-Date: 2015-03-30 00:10-0400\nLast-Translator: Keith <keith@localhost>\nLanguage-Team: German\nLanguage: de\nMIME-Version: 1.0\nContent-Type: text/plain; charset=UTF-8\nContent-Transfer-Encoding: 8bit\nPlural-Forms: nplurals=2; plural=(n != 1);\n",
 'Who wrote this blog entry?' => 'Wer schrieb diesen Blogeintrag?',
 'Reduces spam by asking the commenter the name of who wrote the article' => 'Reduziert Spam durch fragen dem Kommentator den Namen des Eintragsauthors.',
 'Anti-spam:  ' => 'Anti-Spam: ',
-'The name of who wrote this entry' => 'Der Name des Menschen, der diesen Eintrag schrieb',
-'You must provide the correct answer to the anti-spam question.' => 'Sie m�ssen die rechte Antwort an die Anti-Spam-Frage geben: ',
+'You must provide the correct answer to the anti-spam question.' => 'Sie müssen die rechte Antwort an die Anti-Spam-Frage geben: ',
+'Questions for all collections' => 'Fragen für alle Kollektionen',
+'Format: Question? ||Answer1|Answer2|...' => 'Format: Frage? ||Antwort1|Antwort2|...',
+'Questions for this collection only' => 'Fragen nur für diese Kollektion',
 
 );
 ?>
--- locales/es_ES/_global.php
+++ locales/es_ES/_global.php
@@ -7,12 +7,14 @@ if( !defined('EVO_MAIN_INIT') ) die( 'Please, do not access this page directly.'
 
 $trans['es_ES'] = array(
 '__meta__' => array('format_version'=>1, 'charset'=>'utf-8'),
-'' => "Project-Id-Version: b 2e\nReport-Msgid-Bugs-To: http://fplanque.net/\nPOT-Creation-Date: 2015-03-29 19:43-0400\nPO-Revision-Date: 2015-03-29 19:59-0400\nLast-Translator: Keith Bowes <keith@localhost>\nLanguage-Team: Spanish\nLanguage: es\nMIME-Version: 1.0\nContent-Type: text/plain; charset=UTF-8\nContent-Transfer-Encoding: 8bit\nPlural-Forms: nplurals=2; plural=(n != 1);\n",
+'' => "Project-Id-Version: b 2e\nReport-Msgid-Bugs-To: http://fplanque.net/\nPOT-Creation-Date: 2015-03-30 00:04-0400\nPO-Revision-Date: 2015-03-30 00:09-0400\nLast-Translator: Keith Bowes <keith@localhost>\nLanguage-Team: Spanish\nLanguage: es\nMIME-Version: 1.0\nContent-Type: text/plain; charset=UTF-8\nContent-Transfer-Encoding: 8bit\nPlural-Forms: nplurals=2; plural=(n != 1);\n",
 'Who wrote this blog entry?' => 'El nombre de quien escribí esa entrada',
 'Reduces spam by asking the commenter the name of who wrote the article' => 'Reduce spam con le pregunta al comentador el nombro de quien escribí el articulo',
 'Anti-spam:  ' => 'Antispam:  ',
-'The name of who wrote this entry' => 'El nombre de quien escribí esa entrada',
 'You must provide the correct answer to the anti-spam question.' => 'Debes proveer el repuesta correcta a la pregunta antispame.',
+'Questions for all collections' => 'Preguntas para todas las colecciones',
+'Format: Question? ||Answer1|Answer2|...' => 'Format: Pregunta? ||Repuesta1|Repuesta2|…',
+'Questions for this collection only' => 'Preguntas para sólo esta colección',
 
 );
 ?>
