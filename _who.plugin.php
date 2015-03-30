0f9d241c74e876752b92bf1d990191baa71fe5c3
Keith Bowes <zooplah@gmail.com>
2015-03-30 00:03:54 -0400

    Changed plugin to a generic CAPTCHA plugin

--- _who.plugin.php
+++ _who.plugin.php
@@ -2,56 +2,141 @@
 
 class who_plugin extends Plugin
 {
-  var $name = 'Who';
-  var $version = '1.0';
+	var $name = 'Who';
+	var $version = '1.0';
 
-  var $code = 'whoplug';
-  var $group = 'antispam';
-  var $number_of_installs = 1;
+	var $code = 'who_plugin';
+	var $group = 'antispam';
+	var $number_of_installs = 1;
 
-  var $apply_rendering = 'never';
+	var $apply_rendering = 'never';
 
-  function PluginInit()
-  {
-	  $this->short_desc = $this->T_('Who wrote this blog entry?');
-	  $this->lon_gdesc = $this->T_('Reduces spam by asking the commenter the name of who wrote the article');
-  }
-
-  private function getInput()
+	function PluginInit()
 	{
-    echo '<div class="label">';
-    echo '<label for="nomo_de_verkisto">' . $this->T_('Anti-spam:  ') . '</label></div>';
-    echo '<div class="input"><input type="text" name="nomo_de_verkisto" id="nomo_de_verkisto" size="40" maxlength="100" />';
-    echo '<span class="note">(' . $this->T_('The name of who wrote this entry') . ')</span>';
+		$this->short_desc = $this->T_('Who wrote this blog entry?');
+		$this->long_desc = $this->T_('Reduces spam by asking the commenter the name of who wrote the article');
+	}
+
+	private function getInput()
+	{
+		$this->parseQuestion($question, $answer);
+		$ans = base64_encode(serialize($answer));
+		echo '<div class="label">';
+		echo '<label for="captcha_antwort">' . $this->T_('Anti-spam:  ') . '</label></div>';
+		echo '<div class="input"><input type="text" name="captcha_antwort" id="captcha_antwort" size="40" maxlength="100" />';
+		echo '<input type="hidden" name="captcha_frage" value="' . $ans . '" />';
+		echo '<span class="note">(' . $question . ')</span>';
 		echo '</div>';
-  }
+	}
 
-  function BeforeCommentFormInsert(& $params)
-  {
-    $comment =& $params['Comment'];
-    $is_preview = $params['is_preview'];
-    $author = $comment->Item->get_creator_User()->firstname;
-    header('Content-type: text/html; charset=utf-8');
+	private function GetQuestions()
+	{
+		global $Blog;
+		$global_questions = explode("\r\n", $this->Settings->get('globfrag'));
+		$local_questions = explode("\r\n", $this->get_coll_setting('ortfrag', $Blog));
+		$questions = array_merge($global_questions, $local_questions);
+		return $questions;
+	}
 
-    if (!$is_preview && !is_logged_in() && (strtolower($_POST['nomo_de_verkisto']) != strtolower($author)))
-    {
-      /* The message type must be 'error' so the operation will be aborted.
-       * Without a type, it defaults to 'note', which won't stop the posting. */
-      $this->msg($this->T_('You must provide the correct answer to the anti-spam question.'), 'error');
-      }
-  }
+	private function getQuestion($qno = -1)
+	{
+		$questions = $this->GetQuestions();
+		if ($qno < 0)
+		{
+			$qno = count($questions);
+			if ($qno > 1)
+				$qno = rand(1, $qno - 1);
+		}
 
-  function DisplayCommentFormFieldset(& $params)
-  {
-    if (!is_logged_in())
-    {
-      $form =& $params['Form'];
+		return $questions[$qno];
+	}
 
-      $form->begin_fieldset(NULL, array('id' => 'who_plugin'));
-      $this->getInput();
-      $form->end_fieldset();
-    }
-  }
+	private function getAnswer($question)
+	{
+		$questions = $this->GetQuestions();
+		foreach ($questions as $q)
+		{
+			if (strpos($q, $question) === 0)
+			{
+				$this->question = $q;
+				break;
+			}
+		}
+		$this->parseQuestion($question, $answer);
+		return $answer;
+	}
+
+	private function parseQuestion(& $question, & $answer)
+	{
+		if (!isset($this->question))
+			$this->question = $this->getQuestion();
+
+		preg_match('/^(.+)\s+\|\|(.+)$/', $this->question, $matches);
+		list($match, $question, $answer) = $matches;
+		$answer = explode('|', $answer);
+	}
+
+	function BeforeCommentFormInsert(& $params)
+	{
+		$frage = unserialize(base64_decode($_POST['captcha_frage']));
+		$comment =& $params['Comment'];
+		$is_preview = $params['is_preview'];
+		header('Content-type: text/html; charset=utf-8');
+
+		foreach ($frage as $answer)
+		{
+			$is_correct = strcasecmp($answer, $_POST['captcha_antwort']) == 0;
+			if ($is_correct) break;
+		}
+
+		if (!$is_preview && !is_logged_in() && !$is_correct)
+		{
+			/* The message type must be 'error' so the operation will be aborted.
+			   * Without a type, it defaults to 'note', which won't stop the posting. */
+			$this->msg($this->T_('You must provide the correct answer to the anti-spam question.'), 'error');
+		}
+	}
+
+	function DisplayCommentFormFieldset(& $params)
+	{
+		if (!is_logged_in())
+		{
+			$form =& $params['Form'];
+
+			$form->begin_fieldset(NULL, array('id' => 'who_plugin'));
+			$this->getInput();
+			$form->end_fieldset();
+		}
+	}
+
+	function GetDefaultSettings(& $params)
+	{
+		return array(
+			'globfrag' => array(
+				'defaultvalue' => '',
+				'label' => $this->T_('Questions for all collections'),
+				'note' => $this->T_('Format: Question? ||Answer1|Answer2|...'),
+				'type' => 'html_textarea',
+			)
+		);
+	}
+
+	function GetDefaultUserSettings(& $params)
+	{
+		return array(
+			'ortfrag' => array(
+				'defaultvalue' => '',
+				'label' => $this->T_('Questions for this collection only'),
+				'note' => $this->T_('Format: Question? ||Answer1|Answer2|...'),
+				'type' => 'html_textarea',
+			  )
+		  );
+	}
+
+	function get_coll_setting_definitions( & $params )
+	{
+		return $this->GetDefaultUserSettings($params);
+	}
 }
 
 ?>
